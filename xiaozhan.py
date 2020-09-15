257885176aa572953b044e9ff68b88fecc86cdf9
binhe <binhetech@163.com>
2020-09-15 22:03:40 +0800

    add Derivatives

--- xiaozhan.py
+++ xiaozhan.py
@@ -13,7 +13,7 @@ class XiaozhanCrawler(object):
         self.url = "http://top.zhan.com/cihui/%s-%s.html"
         self.countryCh2En = {"美": "US", "英": "UK"}
         self.inflectionCh2En = {"复数": "", "过去式": "", "过去分词": "", "现在分词": "", "第三人称单数": ""}
-        self.items = ["PhoneticSymbols", "ParaPhrases", "Inflections", "Collocations"]
+        self.items = ["PhoneticSymbols", "ParaPhrases", "Inflections", "Collocations", "Derivatives"]
         self.dictPath = "./output/dict/xiaozhan/"
         if not os.path.exists(self.dictPath):
             os.makedirs(self.dictPath)
@@ -27,6 +27,7 @@ class XiaozhanCrawler(object):
             return self.read_infos(lexicon)
         else:
             url = self.url % ("ielts", lexicon)
+            response = requests.get(url)
             html = etree.parse(url, etree.HTMLParser(encoding="utf-8"))
             if len(lexicon.split()) > 1:
                 lexiconType = "Phrase"
@@ -37,6 +38,9 @@ class XiaozhanCrawler(object):
                 try:
                     if k == "PhoneticSymbols":
                         result[k] = eval("self.get_%s(lexicon)" % k)
+                    elif k == "Derivatives":
+                        id = html.xpath("//body")[0].attrib["data-word_id"]
+                        result[k] = eval("self.get_%s(id, lexicon)" % k)
                     else:
                         result[k] = eval("self.get_%s(html)" % k)
                 except Exception as e:
@@ -122,23 +126,23 @@ class XiaozhanCrawler(object):
                 # 添加例句
                 sentences = []
                 if len(jianmingSentEns) == len(jianmingSentChs):
-                    sentences += [{"english": e, "chinese": c, "source": "jianming", "audioUrlUS": "", "audioUrlUK": ""}
-                                  for
-                                  e, c in zip(jianmingSentEns, jianmingSentChs)]
+                    sentences += [{"english": e, "chinese": c, "source": self.source + "-jianming", "audioUrlUS": "",
+                                   "audioUrlUK": ""}
+                                  for e, c in zip(jianmingSentEns, jianmingSentChs)]
                 if len(sceneSentEns) == len(sceneSentChs):
-                    sentences += [{"english": e, "chinese": c, "source": "scene", "audioUrlUS": "", "audioUrlUK": ""}
-                                  for
-                                  e, c in zip(sceneSentEns, sceneSentChs)]
+                    sentences += [{"english": e, "chinese": c, "source": self.source + "-scene", "audioUrlUS": "",
+                                   "audioUrlUK": ""}
+                                  for e, c in zip(sceneSentEns, sceneSentChs)]
                 if len(toeflSentEns) == len(toeflSentChs):
-                    sentences += [{"english": e, "chinese": c, "source": name, "audioUrlUS": "", "audioUrlUK": ""}
-                                  for
-                                  e, c in zip(toeflSentEns, toeflSentChs)]
+                    sentences += [{"english": e, "chinese": c, "source": self.source + "-" + name, "audioUrlUS": "",
+                                   "audioUrlUK": ""}
+                                  for e, c in zip(toeflSentEns, toeflSentChs)]
                 paraphrase = {"pos": pos, "english": "", "chinese": parapch, "Sentences": sentences,
                               "source": self.source}
 
                 paraphrases.append(paraphrase)
             except Exception as e:
-                print(repr(e))
+                print("Error: {}".format(repr(e)))
                 pass
         return paraphrases
 
@@ -154,7 +158,7 @@ class XiaozhanCrawler(object):
             out[n] = w.strip()
         return out
 
-    def get_collocations(self, html):
+    def get_fixed_collocations(self, html):
         """
         固定搭配提取.
         """
@@ -183,16 +187,41 @@ class XiaozhanCrawler(object):
         搭配提取.
         """
         # 固定搭配
-        outs = self.get_collocations(html)
+        outs = self.get_fixed_collocations(html)
         # 习惯用法
         # outs += self.get_idiomatic_usage(html)
         return outs
 
+    def get_Derivatives(self, id, lexicon):
+        """
+        派生词提取.
+        """
+        outs = []
+        html = etree.parse(f"http://top.zhan.com/vocab/detail/one-2-ten.html?test_type=2&word_id={id}&word={lexicon}",
+                           etree.HTMLParser(encoding="utf-8"))
+        ens = html.xpath("//p[@class='cssDeriWordsBoxId']/text()")
+        chs = html.xpath("//ul[@class='cssDeriWordsBoxType']/li/text()")
+        assert len(ens) == len(chs)
+        for en, ch in zip(ens, chs):
+            para = ch.split(".", 1)
+            if len(para) != 2:
+                continue
+            pos = para[0].strip() + "."
+            parapch = para[1].strip()
+            outs.append({"Lexicon": en.strip(), "chinese": parapch, "pos": pos, "source": self.source})
+        return outs
+
     def save_infos(self, lexicon, infos):
+        """
+        保存词汇信息.
+        """
         with open(self.dictPath + str(lexicon), "w", encoding="utf-8") as f:
             json.dump(infos, f, ensure_ascii=False)
 
     def read_infos(self, lexicon):
+        """
+        读取词汇信息.
+        """
         with open(self.dictPath + str(lexicon), "r", encoding="utf-8") as f:
             return json.load(f)
 
@@ -202,10 +231,12 @@ if __name__ == "__main__":
     import pandas as pd
     from multiprocessing import Pool
 
-    words = pd.read_csv(r"D:\work\database\单词-缺失美英音标-汇总2-sorted.csv", header=None)
-    print("words shape={}".format(words.shape))
+    c.get_infos("taste")
 
-    p = Pool(16)
-    words = words[0].values
-    for i in range(len(words)):
-        p.apply(c.get_infos, (words[i],))
+    # words = pd.read_csv(r"D:\work\database\单词-缺失美英音标-汇总2-sorted.csv", header=None)
+    # print("words shape={}".format(words.shape))
+    #
+    # p = Pool(1)
+    # words = words[0].values
+    # for i in range(len(words)):
+    #     p.apply(c.get_infos, (words[i],))
